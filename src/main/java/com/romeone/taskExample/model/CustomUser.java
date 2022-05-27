755e6e9d6986ae8e52c058de04e74b5c12069ad1
rbloechlinger <rbloechlinger@gmail.com>
2022-05-27 16:33:09 +0200

    valid build

--- src/main/java/com/romeone/taskExample/model/CustomUser.java
+++ src/main/java/com/romeone/taskExample/model/CustomUser.java
@@ -1,10 +1,19 @@
 package com.romeone.taskExample.model;
 
+import java.sql.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.Email;
 
 @Entity
 @Table(name = "custom_user")
@@ -13,8 +22,99 @@ public class CustomUser {
     @GeneratedValue
     private Long id;
 
+    @NotNull(message = "Name cannot be null")
     @Column(name = "name", nullable = false)
     private String name;
+
+    @Size(min = 0, max = 80, message = "About Me must be between 10 and 80 characters")
+    @Column(name = "first_name", length = 80)
+    private String firstName;
+
+    @Email(message = "Email should be valid")
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "birthday")
+    private Date birthday;
+
+    @Column(name = "gender", length = 50)
+    private String gender;
+
+    @Column(name = "profession")
+    private String profession;
+
+    @Column(name = "married")
+    private Boolean married;
+
+    @Column(name = "note")
+    private String note;
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public Boolean getMarried() {
+        return married;
+    }
+
+    public void setMarried(Boolean married) {
+        this.married = married;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
     public String getName() {
         return name;
