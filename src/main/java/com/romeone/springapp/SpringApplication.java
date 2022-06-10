bae8c5ede9cd8bdc222e53f7d0d390448765809d
rbloechlinger <rbloechlinger@gmail.com>
2022-06-10 13:46:16 +0200

    Update SpringApplication.java

--- src/main/java/com/romeone/springapp/SpringApplication.java
+++ src/main/java/com/romeone/springapp/SpringApplication.java
@@ -2,10 +2,8 @@ package com.romeone.springapp;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,46 +16,29 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @SpringBootApplication
 public class SpringApplication {
 
-	private final Logger logger = LoggerFactory.getLogger(SpringApplication.class);
+    private final Logger logger = LoggerFactory.getLogger(SpringApplication.class);
 
-	public static void main(String[] args) {
-		org.springframework.boot.SpringApplication.run(SpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(SpringApplication.class, args);
+    }
 
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return new AuthenticationManager() {
+            @Override
+            public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+                return null;
+            }
+        };
+    }
 
-//			String[] beanNames = ctx.getBeanDefinitionNames();
-//			Arrays.sort(beanNames);
-//			for (String beanName : beanNames) {
-//				System.out.println(beanName);
-//			}
-
-		};
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return new AuthenticationManager() {
-			@Override
-			public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-				return null;
-			}
-		};
-	}
-
-	@EventListener(ApplicationReadyEvent.class)
-	public void runAfterStartup() {
-//		queryAllCustomers();
-//		createCustomer();
-//		queryAllCustomers();
-	}
+    @EventListener(ApplicationReadyEvent.class)
+    public void runAfterStartup() {
+        // TODO: Sample data
+    }
 }
